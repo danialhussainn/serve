7502a38c6cd62650c5b75f1af072287473b669a3
danialhussainn <danialhussainn@gmail.com>
2020-07-25 00:57:43 -0500

    updated form

---
+++ clients/app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import SignUp from './componenet/form';
+
+
+function App() {
+  return (
+    <div >
+      <SignUp/>
+     
+    </div>
+  );
+}
+
+export default App;
